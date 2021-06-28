5ee18c3a6dd0325fd78f69e8abc510249410d88f
sedera_nomentsoa <starksedera@gmail.com>
2021-06-28 11:28:25 +0300

    parametrage server avec mongodb

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const uri = 'mongodb+srv://sedera:sederamongodb@cluster0.sqoyq.mongodb.net/nodeTptParie?retryWrites=true&w=majority';
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+const app = express();
+
+mongoose.connect(uri, options)
+    .then(() => {
+        console.log("TPT node parie est connecté à mongodb :p ");
+        console.log("uri utilisé: " + uri);
+    },
+        err => {
+            console.log("Une Erreur de connexion à la base mongodb", err);
+        });
+
+
+// pour le CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// pour les formulaire
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+
+module.exports = app;
---
+++ models/Utilisateur.js
@@ -0,0 +1,19 @@
+const mongoose = require(mongoose)
+
+const userSchema = mongoose.Scheme({
+    login: { type: String, required: true },
+    password: { type: String, required: true },
+    nom: { type: String, required: true },
+    prenom: { type: String, required: true },
+    etat: { type: String, required: true },
+    email: { type: String, required: true },
+    numeroTelephone: { type: String, required: true },
+    role:{
+        id: { type: Number, require: true },
+        nom: { type: String, require: true },
+        rang: { type: String, require: true }
+    },
+    imageProfil: { type: String, required: false}
+});
+
+module.exports = mongoose.model('Utilisateur', userSchema);
