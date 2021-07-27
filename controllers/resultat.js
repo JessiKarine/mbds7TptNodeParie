4790f723396059aa27a6d92db2db35a3b9f56731
Razakason Jessi <razakasonjessi@gmail.com>
2021-07-27 15:53:55 +0300

    [dev][resultat]filter by categorie

--- controllers/resultat.js
+++ controllers/resultat.js
@@ -3,7 +3,89 @@ const Resultat = require('../models/Resultat');
 
 async function getResultatParCategorie(req,res) {
   try{
+    var categorie = req.query.nomcategorie;
+    let val = null ; 
+    if(!categorie || typeof categorie ==="undefined") { 
+      val = await getResultatParToutCategorie(req);
+    }
+    else { 
+      val = await getResultatParUnCategorie(req,categorie);
+    }
+    res.send(val);
+  }
+  catch(e) { 
+    res.send(e)
+  }   
+}
+
+async function getResultatParToutCategorie(req) { 
+    try { 
     var aggregateQuery = Resultat.aggregate(
+        [
+            {
+                $unwind : '$idmatch' // destructurer le tableau venant des documents , nom du colonne
+            },
+            {
+                $lookup : {
+                    from : 'matchs', // nom du table avec 's' parce que c'est généré automatiquement par le cloudATlas (mongodb)
+                    localField : 'idmatch',
+                    foreignField : '_id' , 
+                    as : 'idmatch'
+                }
+            },
+            {
+              $addFields : { 
+                  "categorie" : "$idmatch.idcategorie.nom", 
+              }
+            }, 
+            {
+                $unwind : "$categorie"
+            },
+            {
+              $project : { 
+                  "categorie" : 1,
+                  "pointequipe1" : 1,
+                  "pointequipe2" : 1 ,
+                  "idmatch" : 1
+              }
+            },
+            {
+              $addFields : { 
+                  "idmatch.pointequipe1" : "$pointequipe1",
+                  "idmatch.pointequipe2" : "$pointequipe2"
+              }
+            },
+            { 
+                $group : { 
+                    _id : "$categorie", 
+                    resultats : {
+                        "$push" : {
+                          $arrayElemAt : ["$idmatch",0]
+                        }
+                    }
+                }
+            }
+            
+            
+        ]
+      
+    );
+    const val = await Resultat.aggregatePaginate(
+        aggregateQuery,
+        {
+          page: parseInt(req.query.page) || 1,
+          limit: parseInt(req.query.limit) || 10,
+        }
+        ) ; 
+        return val;
+  }
+  catch(e){
+    console.log(e);
+  }
+}
+async function getResultatParUnCategorie(req,nom) { 
+  try { 
+  var aggregateQuery = Resultat.aggregate(
       [
           {
               $unwind : '$idmatch' // destructurer le tableau venant des documents , nom du colonne
@@ -15,6 +97,11 @@ async function getResultatParCategorie(req,res) {
                   foreignField : '_id' , 
                   as : 'idmatch'
               }
+          },
+          {
+            $match : {
+                "idmatch.idcategorie.nom" : `${nom}`
+            }
           },
           {
             $addFields : { 
@@ -48,23 +135,23 @@ async function getResultatParCategorie(req,res) {
                   }
               }
           }
-           
+          
           
       ]
     
   );
   const val = await Resultat.aggregatePaginate(
-    aggregateQuery,
-    {
-      page: parseInt(req.query.page) || 1,
-      limit: parseInt(req.query.limit) || 10,
-    }
-    ) ; 
-    res.send(val);
-  }
-  catch(e){
-    console.log(e);
-  }
+      aggregateQuery,
+      {
+        page: parseInt(req.query.page) || 1,
+        limit: parseInt(req.query.limit) || 10,
+      }
+      ) ;
+      return val;
+}
+catch(e){
+  console.log(e);
+}
 }
 function getResultat(req, res) {
     var aggregateQuery = Resultat.aggregate(
